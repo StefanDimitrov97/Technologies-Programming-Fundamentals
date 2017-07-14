9378aceb5c63c2a389fd94621a7008012131c177
StefanDimitrov97 <stefan100297@abv.bg>
2017-07-14 16:31:29 +0300

    Exercises CSharp Intro and Basic Syntax

---
+++ Exercises CSharp Intro and Basic Syntax/Debit Card Number/Debit Card Number/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercises_CSharp_Intro_and_Basic_Syntax
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int one = int.Parse(Console.ReadLine());
+            int two = int.Parse(Console.ReadLine());
+            int three = int.Parse(Console.ReadLine());
+            int four = int.Parse(Console.ReadLine());
+
+            Console.Write("{0:D4} {1:D4} {2:D4} {3:D4}", one,two,three,four);
+
+        }
+    }
+}
