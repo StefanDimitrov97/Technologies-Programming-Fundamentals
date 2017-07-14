12168bf5516b5ad531a849989302ad9150dfde4b
StefanDimitrov97 <stefan100297@abv.bg>
2017-07-14 16:23:15 +0300

    C# Conditional Statements and Loops - Lab

---
+++ C# Conditional Statements and Loops - Lab/Problem 1. Choose a Drink/Problem 1. Choose a Drink/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem_1.Choose_a_Drink
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string professions = Console.ReadLine();
+
+            if (professions == "Athlete")
+            {
+                Console.WriteLine("Water");
+            }
+            else if (professions == "Businessman" || professions == "Businesswoman")
+            {
+                Console.WriteLine("Coffee");
+            }
+            else if (professions == "SoftUni Student")
+            {
+                Console.WriteLine("Beer");
+            }
+            else
+            {
+                Console.WriteLine("Tea");
+            }
+        }
+    }
+}
